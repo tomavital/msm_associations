a6893ff8757b80ee8941a8aaaf3ae6b5d9a4b505
rails -v <c:sites>
2017-05-12 17:12:11 -0500

    sffs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512014039) do
+ActiveRecord::Schema.define(version: 20170512215541) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "dob"
+    t.text     "bio"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string   "name"
