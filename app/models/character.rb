ce5ef67994b05fb78338664c756f3b84fe33319c
rails -v <c:sites>
2017-05-16 13:20:05 -0500

    cxcdsds
    xccx

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
-
+has_many :movies, :through => :characters
 belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
 #   Character:
 #  - movie_id: must be present
