7a37a80a1e5336e08f5cc9f3abfc513b13711ecc
rails -v <c:sites>
2017-05-16 13:34:45 -0500

    cxcxcx

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ApplicationRecord
 has_many :movies, :through => :characters
 belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
 #   Character:
 #  - movie_id: must be present
 validates :movie_id, :presence => true
